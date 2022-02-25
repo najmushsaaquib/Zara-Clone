6e1fc2d127d0ed364401f2e86b6990051cebb28f
ArunrajMK <arunrajshanker6@gmail.com>
2022-02-25 21:05:37 +0530

    Media Queries Modified

--- Script/address&payment.js
+++ Script/address&payment.js
@@ -1,10 +1,3 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
 
 //   document.querySelector("1st").addEventListener("click",Optfunction)
 
